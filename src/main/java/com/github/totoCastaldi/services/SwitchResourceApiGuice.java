c67c977c42161c3ac63125a9bba874d5336d4aa9
Toto Castaldi <toto.castaldi@gmail.com>
2015-11-12 14:31:45 +0100

    mashape security token

--- src/main/java/com/github/totoCastaldi/services/SwitchResourceApiGuice.java
+++ src/main/java/com/github/totoCastaldi/services/SwitchResourceApiGuice.java
@@ -1,22 +1,31 @@
 package com.github.totoCastaldi.services;
 
-import com.github.totoCastaldi.restServer.ApiGuice;
+import com.github.totoCastaldi.restServer.ApiServletContextListener;
 import com.github.totoCastaldi.restServer.model.CustomerDao;
+import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.util.List;
 
 /**
  * Created by toto on 09/11/15.
  */
 
-public class SwitchResourceApiGuice extends ApiGuice {
+public class SwitchResourceApiGuice extends ApiServletContextListener {
 
     public SwitchResourceApiGuice() {
     }
 
     @Override
-    protected Package getPackage() {
-        return SwitchResource.class.getPackage();
+    protected List<Class<? extends ContainerRequestFilter>> getContainerRequestFilters() {
+        return Lists.newArrayList(MashapeHeaderCheck.class);
+    }
+
+    @Override
+    protected List<Package> getPackages() {
+        return Lists.newArrayList(SwitchResource.class.getPackage());
     }
 
     @Override
