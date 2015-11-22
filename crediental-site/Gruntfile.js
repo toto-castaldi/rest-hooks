76c7929b9cb100a9ac5d978c4eb610e57f6f11fe
toto castaldi <toto.castaldi@gmail.com>
2015-11-22 10:32:42 +0100

    site init

---
+++ crediental-site/Gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+
+  require('load-grunt-tasks')(grunt);
+
+  var config = {
+    app: 'app'
+  };
+
+  grunt.initConfig({
+
+    config: config,
+
+    watch: {
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.app %>/{,*/}*.html'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      }
+    },
+
+    clean: {
+      server: '.tmp'
+    }
+  });
+
+  grunt.registerTask('default', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+    if (grunt.option('allow-remote')) {
+      grunt.config.set('connect.options.hostname', '0.0.0.0');
+    }
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+};
