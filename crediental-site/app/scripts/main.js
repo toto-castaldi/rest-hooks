ac628afb7699c2b2b6e24bf0d6c7f99eeab154b7
Toto Castaldi <toto.castaldi@gmail.com>
2015-11-26 10:43:21 +0100

    recovery password

--- crediental-site/app/scripts/main.js
+++ crediental-site/app/scripts/main.js
@@ -28,52 +28,138 @@ $().ready(
           }
         };
 
+        var formLoading = function (subSectionId) {
+          $('#' + subSectionId + ' form input[type=submit]').isLoading('');
+        };
 
-    		$('#links a[href=#signup]').click(function () {
-    			$('#signup').removeClass('hidden');
+        var formLoaded = function (subSectionId) {
+          $('#' + subSectionId + ' form input[type=submit]').isLoading('hide');
+        };
+
+        var openSubSection = function (subSectionId) {
+          $('.subsection').hide();
+          $('#' + subSectionId).show();
+        };
+
+        var closeAllSubsection = function () {
+          $('.subsection').hide();
+        };
+
+        $('.subsection').each(function (index, subsection) {
+          var id = $(subsection).attr('id');
+          $('#links a[href=#' + id + ']').click(function () {
+            openSubSection(id);
+          });
+        });
+    		if (window.location.hash !== '' && window.location.hash.indexOf('&') === -1) {
+          closeAllSubsection();
+    			$(window.location.hash).show();
+    		}
+
+        var genericError = function (subSectionId) {
+          return function (XMLHttpRequest, textStatus, errorThrown) {
+          trace('error', XMLHttpRequest, textStatus, errorThrown);
+          if (subSectionId) {
+            formLoaded(subSectionId);
+          }
+
+          if (XMLHttpRequest.readyState === 4) {
+                // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
+                notie.alert(3, 'Error (' + XMLHttpRequest.status + ', ' + XMLHttpRequest.statusText + ')', 5);
+            }
+            else if (XMLHttpRequest.readyState === 0) {
+                // Network error (i.e. connection refused, access denied due to CORS, etc.)
+                notie.alert(3, 'Network Error', 5);
+            }
+            else {
+                // something weird is happening
+                notie.alert(3, 'Error !', 5);
+            }
+          };
+        };
+
+
+        //confirmation email
+    		if (window.location.hash.startsWith('#confirm?')) {
+          (function () {
+      			var email = getParameterByName('e');
+      			var token = getParameterByName('t');
+
+      			trace('confirm ' + email + ' with token ' + token);
+
+      			$.ajax({
+      				type: 'PUT',
+      				url: buildUrl('confirmToken') + '/' + token,
+      				data: JSON.stringify({
+      				    'email' : email
+      				}),
+      				success: function (response) {
+      					trace(response);
+                notie.alert(1, 'Success! You are confirmed', 1.5);
+      				},
+              error : genericError,
+      				contentType:'application/json;charset=UTF-8',
+      			  	dataType: 'json'
+      			});
+          })();
+    		}
+
+        //reset password open e prefill
+    		if (window.location.hash.startsWith('#reset-password?')) {
+          (function () {
+      			var email = getParameterByName('e');
+      			var token = getParameterByName('t');
+
+      			trace('password-lost ' + email + ' with token ' + token);
+
+            $('#reset-password form input[name=email]').val(email);
+            $('#reset-password form input[name=token]').val(token);
+
+            openSubSection('reset-password');
+          }());
+    		}
+
+        //reset password submit
+        $('#reset-password form').validate({
+          rules : {
+            password : {
+              required : true
+            }
+          }
+        });
+
+    		$('#reset-password form').submit(function( event ) {
+          if ($(event.target).valid()) {
+      			var email = $('#reset-password form input[name=email]').val();
+      			var token = $('#reset-password form input[name=token]').val();
+      			var password = $('#reset-password form input[name=password]').val();
+
+            formLoading('reset-password');
+
+      			$.ajax({
+              timeout: 20000,
+              cache : false,
+      				type: 'PUT',
+      				url: buildUrl('lostToken/' + token),
+      				data: JSON.stringify({
+      				    'email' : email,
+      				    'password' : password
+      				}),
+      				success: function (response) {
+      					trace(response);
+                notie.alert(1, 'Password correctly changed', 1.5);
+                formLoaded('reset-password');
+                closeAllSubsection();
+      				},
+              error : genericError('signup'),
+      				contentType:'application/json;charset=UTF-8',
+      			  dataType: 'json'
+      			});
+          }
+    			event.preventDefault();
     		});
 
-    		if (window.location.hash !== '' && window.location.hash.indexOf('&') === -1) {
-    			$(window.location.hash).removeClass('hidden');
-    		}
-
-    		if (window.location.hash.startsWith('#confirm?')) {
-    			var email = getParameterByName('e');
-    			var token = getParameterByName('t');
-
-    			trace('confirm ' + email + ' with token ' + token);
-
-    			$.ajax({
-    				type: 'PUT',
-    				url: buildUrl('confirmToken') + '/' + token,
-    				data: JSON.stringify({
-    				    'email' : email
-    				}),
-    				success: function (response) {
-    					trace(response);
-              notie.alert(1, 'Success! You are confirmed', 1.5);
-    				},
-            error : function (XMLHttpRequest, textStatus, errorThrown) {
-              trace('error', XMLHttpRequest, textStatus, errorThrown);
-
-              if (XMLHttpRequest.readyState === 4) {
-                    // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
-                    notie.alert(3, 'Error (' + XMLHttpRequest.status + ', ' + XMLHttpRequest.statusText + ')', 5);
-                }
-                else if (XMLHttpRequest.readyState === 0) {
-                    // Network error (i.e. connection refused, access denied due to CORS, etc.)
-                    notie.alert(3, 'Network Error', 5);
-                }
-                else {
-                    // something weird is happening
-                    notie.alert(3, 'Error !', 5);
-                }
-            },
-    				contentType:'application/json;charset=UTF-8',
-    			  	dataType: 'json'
-    			});
-    		}
-
+        //signup
         $('#signup form').validate({
           rules : {
             email : {
@@ -91,7 +177,7 @@ $().ready(
       			var email = $('#signup form input[name=email]').val();
       			var password = $('#signup form input[name=password]').val();
 
-            $('#signup form input[type=submit]').isLoading('');
+            formLoading('signup');
 
       			$.ajax({
               timeout: 20000,
@@ -107,26 +193,50 @@ $().ready(
       				success: function (response) {
       					trace(response);
                 notie.alert(1, 'Success! Check you email', 1.5);
-                $('#signup form input[type=submit]').isLoading('hide');
-      					$('#signup').addClass('hidden');
+                formLoaded('signup');
+                closeAllSubsection();
       				},
-              error : function (XMLHttpRequest, textStatus, errorThrown) {
-                trace('error', XMLHttpRequest, textStatus, errorThrown);
-                $('#signup form input[type=submit]').isLoading('hide');
+              error : genericError('signup'),
+      				contentType:'application/json;charset=UTF-8',
+      			  dataType: 'json'
+      			});
+          }
+    			event.preventDefault();
+    		});
 
-                if (XMLHttpRequest.readyState === 4) {
-                      // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
-                      notie.alert(3, 'Error (' + XMLHttpRequest.status + ', ' + XMLHttpRequest.statusText + ')', 5);
-                  }
-                  else if (XMLHttpRequest.readyState === 0) {
-                      // Network error (i.e. connection refused, access denied due to CORS, etc.)
-                      notie.alert(3, 'Network Error', 5);
-                  }
-                  else {
-                      // something weird is happening
-                      notie.alert(3, 'Error !', 5);
-                  }
-              },
+        //password-lost
+        $('#password-lost form').validate({
+          rules : {
+            email : {
+              required : true,
+              email : true
+            }
+          }
+        });
+
+    		$('#password-lost form').submit(function( event ) {
+          if ($(event.target).valid()) {
+      			var email = $('#password-lost form input[name=email]').val();
+
+            formLoading('password-lost');
+
+      			$.ajax({
+              timeout: 20000,
+              cache : false,
+      				type: 'POST',
+      				url: buildUrl('lostToken'),
+      				data: JSON.stringify({
+      				    'email' : email,
+      				    'baseUrl' :  document.location.protocol + '//' + document.location.host + document.location.pathname + '#reset-password',
+      				    'skipEmailSend' : false
+      				}),
+      				success: function (response) {
+      					trace(response);
+                notie.alert(1, 'Check you email', 1.5);
+                formLoaded('password-lost');
+                closeAllSubsection();
+      				},
+              error : genericError('password-lost'),
       				contentType:'application/json;charset=UTF-8',
       			  dataType: 'json'
       			});
