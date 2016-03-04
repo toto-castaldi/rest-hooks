659c544122ecc13fa7b9b85cf5bc03f0be869434
Toto Castaldi <toto.castaldi@gmail.com>
2016-03-04 15:46:13 +0100

    supporting plus in email

--- java-server/src/main/java/com/github/totoCastaldi/services/MashapeCredentialResource.java
+++ java-server/src/main/java/com/github/totoCastaldi/services/MashapeCredentialResource.java
@@ -1,17 +1,17 @@
 package com.github.totoCastaldi.services;
 
-import com.github.totoCastaldi.restServer.UserType;
 import com.github.totoCastaldi.restServer.authorization.AuthorizationResult;
 import com.github.totoCastaldi.restServer.authorization.BasicAuthorization;
-import com.google.common.base.Optional;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpStatus;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by toto on 29/11/15.
@@ -22,13 +22,15 @@ public class MashapeCredentialResource implements BasicAuthorization {
     @Override
     public AuthorizationResult checkCredential(String username, String password) {
         try {
-            HttpResponse<JsonNode> response = Unirest.get("https://toto-credential-v1.p.mashape.com/user/" + username + "/" + password)
+            String resourceCredential = "https://toto-credential-v1.p.mashape.com/user/" + URLEncoder.encode(username, "UTF-8") + "/" + password;
+
+            HttpResponse<JsonNode> response = Unirest.get(resourceCredential)
                     .header("X-Mashape-Key", "FCKFcesv9PmshjpunUVhQVx88GI7p1HDYROjsnTmJ45NAYWEnd")
                     .asJson();
 
             final JsonNode body = response.getBody();
 
-            log.info("authentication {} {}", response.getStatus(),  body.toString());
+            log.info("authentication GET {} {} {}", resourceCredential, response.getStatus(),  body.toString());
 
             if (response.getStatus() == HttpStatus.SC_OK) {
                 return AuthorizationResult.passed();
@@ -36,6 +38,9 @@ public class MashapeCredentialResource implements BasicAuthorization {
                 return AuthorizationResult.notPassed("manage your credential here http://toto-castaldi.github.io/rest-switch-resource/");
             }
         } catch (UnirestException e) {
+            log.error(StringUtils.EMPTY, e);
+            return AuthorizationResult.notPassed("manage your credential here http://toto-castaldi.github.io/rest-switch-resource/");
+        } catch (UnsupportedEncodingException e) {
             log.error(StringUtils.EMPTY, e);
             return AuthorizationResult.notPassed("manage your credential here http://toto-castaldi.github.io/rest-switch-resource/");
         }
